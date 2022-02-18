b3e690769bb80b44799f7d60f101e12ab14636d9
Tim GF63 <tim@shatzir.com>
2022-02-18 19:00:14 +0900

    182

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def run_vasya(x,y,angle):
     mariomex=wrap.sprite.get_x(mario)
     mariomey=wrap.sprite.get_y(mario)
 
-    text=wrap.sprite.add_text("run vasya run ",mariomex,mariomey-50,"Arial")
-    time.sleep(2)
-    wrap.sprite.remove(text)
+    #text=wrap.sprite.add_text("run vasya run",mariomex,mariomey-50,"Arial")
+    #time.sleep(2)
+    #wrap.sprite.remove(text)
 
     wrap.sprite.set_size(mario,40,40)
 
@@ -31,20 +31,41 @@ def monstr():
     enemymex=wrap.sprite.get_x(enemy)
     enemymey=wrap.sprite.get_y(enemy)
 
-    text=wrap.sprite.add_text("иди сюда!!!",enemymex,enemymey-50,"Arial")
-    time.sleep(2)
-    wrap.sprite.remove(text)
+   # text=wrap.sprite.add_text("иди сюда!!!",enemymex,enemymey-50,"Arial")
+   #  time.sleep(2)
+   #  wrap.sprite.remove(text)
 
     mariohuntx=wrap.sprite.get_x(mario)
     mariohunty=wrap.sprite.get_y(mario)
 
     wrap.actions.move_to(enemy,mariohuntx-40,mariohunty-40)
 
+def saymonstr(player_id):
+    enemymex=wrap.sprite.get_x(enemy)
+    enemymey=wrap.sprite.get_y(enemy)
+    textenemy = wrap.sprite.add_text("иди сюда!!!", enemymex, enemymey - 50, "Arial",font_size=20,underline=True,text_color=(254, 110, 159))
+    time.sleep(2)
+    wrap.sprite.set_size_percent(textenemy, 200, 200)
+    time.sleep(1)
+    wrap.sprite.set_size_percent(textenemy, 100, 100)
+    time.sleep(2)
+    wrap.sprite.remove(textenemy)
+
+
+def saymario():
+
+
+saymario()
 run_vasya(340,240,180)
+saymonstr()
 monstr()
+saymario()
 run_vasya(150,340,270)
+saymonstr()
 monstr()
+saymario()
 run_vasya(440,540,90)
+saymonstr()
 monstr()
 
 
